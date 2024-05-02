f7b41b4ca08bdccbf0d82cb16af5b67282a1a1ba
Iryu-0817 <iryu20030817@gmail.com>
2024-05-02 11:03:21 +0900

    マップ表示して検索できる画面を追加

---
+++ cafe-search/resources/views/List/map-list.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+        <html lang="jp">
+
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Cafe Search</title>
+                <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+                <link rel="stylesheet" href="{{ asset('css/top-page.css') }}">
+
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto Sans:weight@400&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" />
+            </head>
+
+            
+            <body>
+                <header>
+                        <div class="cafe-search-parent">
+                            <d class="cafe-search">Cafe Search</d>
+
+                            <!-- ハンバーガーメニュー -->
+                            <div class="hotdog">
+
+                                <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+                                <input id="drawer_input" class="drawer_hidden" type="checkbox">
+                                
+                                <!-- ハンバーガーアイコン -->
+                                <label for="drawer_input" class="drawer_open"><span></span></label>
+
+                                <!-- メニュー -->
+                                <nav class="nav_content">
+                                <ul class="nav_list">
+                                    <li class="nav_item"><a href="">Home</a></li>
+                                    <li class="nav_item"><a href="">About Service</a></li>
+                                    <li class="nav_item"><a href="">Profile</a></li>
+                                </ul>
+                                </nav>
+                            </div>
+                        </div>
+                </header>
+
+                
+            </body>
+
+            <footer>
+            </footer>
+
+        </html>
